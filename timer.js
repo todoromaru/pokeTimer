b8bac13ace51c6fa7178c97de383b23097919554
todoromaru <154855750+todoromaru@users.noreply.github.com>
2023-12-26 17:43:10 +0900

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,46 +1,27 @@
 let timer;
 let seconds = 0; // 初期値は0秒
+let alertSetTimes = { 'alert1min': 60, 'alert5min': 300, 'alert10min': 600, 'alert15min': 900 }; // アラーム時間を秒で設定
 
-document.getElementById('startButton').addEventListener('click', function() {
-    unlockAudio();
-    startTimer();
-});
+document.getElementById('startButton').addEventListener('click', startTimer);
 document.getElementById('stopButton').addEventListener('click', stopTimer);
 document.getElementById('resetButton').addEventListener('click', resetTimer);
 document.getElementById('setTimerButton').addEventListener('click', setTimer);
 
-function unlockAudio() {
-    document.querySelectorAll('audio').forEach(audio => {
-        audio.play().then(() => {
-            audio.pause();
-            audio.currentTime = 0;
-        }).catch(() => {});
-    });
-    // イベントリスナーを削除
-    document.body.removeEventListener('touchstart', unlockAudio, false);
-}
-
-document.body.addEventListener('touchstart', unlockAudio, false);
-
-function updateDisplay() {
-    let minutes = Math.floor(seconds / 60);
-    let remainingSeconds = seconds % 60;
-    document.getElementById('timerDisplay').textContent = `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-}
+// ページがロードされた時にタイマーの表示を更新
+updateDisplay();
 
 function startTimer() {
-    if (!timer) {
-        timer = setInterval(() => {
-            if (seconds > 0) {
-                seconds--;
-                updateDisplay();
-                checkAlerts();
-            } else {
-                stopTimer();
-                playFinalAlertSound();
-            }
-        }, 1000);
-    }
+    stopTimer(); // 既存のタイマーを停止（もしあれば）
+    timer = setInterval(() => {
+        if (seconds > 0) {
+            seconds--;
+            updateDisplay();
+            checkAlerts();
+        } else {
+            stopTimer();
+            playSound('finalAlertSound'); // 最終アラーム
+        }
+    }, 1000);
 }
 
 function stopTimer() {
@@ -52,32 +33,54 @@ function resetTimer() {
     stopTimer();
     seconds = 0;
     updateDisplay();
+    resetAlerts();
 }
 
 function setTimer() {
-    let minutes = parseInt(document.getElementById('minutesInput').value);
+    let minutes = parseInt(document.getElementById('minutesInput').value, 10);
     if (!isNaN(minutes) && minutes > 0) {
         seconds = minutes * 60;
         updateDisplay();
-        autoCheckAlerts(minutes);
+        setAlerts(minutes);
     }
 }
 
+function updateDisplay() {
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
+    document.getElementById('timerDisplay').textContent = `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
+
 function checkAlerts() {
-    // 中間アラームのチェックと音声再生
+    for (let alert in alertSetTimes) {
+        if (seconds === alertSetTimes[alert] && document.getElementById(alert).checked) {
+            playSound(alert + 'Sound');
+        }
+    }
 }
 
-function playFinalAlertSound() {
-    const finalAlertSound = document.getElementById('finalAlertSound');
-    finalAlertSound.play();
+function playSound(soundId) {
+    const sound = document.getElementById(soundId);
+    if (sound) {
+        sound.play();
+    }
 }
 
-function autoCheckAlerts(minutes) {
-    // 中間アラームの自動チェックロジック
-    document.getElementById('alert1min').checked = minutes >= 1;
-    document.getElementById('alert5min').checked = minutes >= 5;
-    document.getElementById('alert10min').checked = minutes >= 10;
-    document.getElementById('alert15min').checked = minutes >= 15;
+function setAlerts(minutes) {
+    // 全てのアラートを一旦リセット
+    for (let alert in alertSetTimes) {
+        document.getElementById(alert).checked = false;
+    }
+    // 設定された時間に応じてアラートをセット
+    for (let alert in alertSetTimes) {
+        if (minutes * 60 >= alertSetTimes[alert]) {
+            document.getElementById(alert).checked = true;
+        }
+    }
 }
 
-updateDisplay();
+function resetAlerts() {
+    for (let alert in alertSetTimes) {
+        document.getElementById(alert).checked = false;
+    }
+}
